32b9d87db213a36a686b2a601c1853043bdd8d45
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-04-29 17:59:03 +0300

    step 6: change logic game

--- src/index.js
+++ src/index.js
@@ -1,41 +1,24 @@
 import readlineSync from 'readline-sync';
 
-let userName = '';
-
-export const getName = () => {
+const runGame = (description, getGameData) => {
   console.log('Welcome to the Brain Games!\n');
-  userName = readlineSync.question('May I have you name? ');
-  return `Hello, ${userName}!\n`;
-};
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!\n`);
+  console.log(description);
 
-const evenNum = (num) => {
-  if (num % 2 === 0) {
-    return 'yes';
-  }
-  return 'no';
-};
+  const attemtCount = 3;
 
-const getRandom = () => {
-  const min = 1;
-  const max = 100;
-  const random = Math.floor(Math.random() * (max - min) + 1);
-  return random;
-};
-
-export const evenGame = () => {
-  console.log(getName());
-  console.log('Answer "yes" if the number is even, otherwise answer "no".\n');
-
-  for (let i = 0; i < 3; i += 1) {
-    const num = getRandom();
-    const checkNum = evenNum(num);
-    console.log(`Question: ${num}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (checkNum === answer) {
+  for (let i = 0; i < attemtCount; i += 1) {
+    const [question, correctAnswer] = getGameData();
+    console.log(`Question: ${question}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
     } else {
-      return `Sorry, "${answer}" is wrong answer ;(. Correct answer was "${checkNum}".\nLet's try again, ${userName}!`;
+      return `Sorry, "${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`;
     }
   }
   return `Congratulations, ${userName}!`;
 };
+
+export default runGame;
