1d90fa4818b41e97fae0073b7adddf1237e1e247
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-05 19:24:10 +0300

    remove 'return', add console.log

--- src/index.js
+++ src/index.js
@@ -1,24 +1,25 @@
 import readlineSync from 'readline-sync';
 
+const attemptsCount = 3;
+
 const runGame = (description, getGameData) => {
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${userName}!\n`);
   console.log(description);
 
-  const attemtCount = 3;
-
-  for (let i = 0; i < attemtCount; i += 1) {
+  for (let i = 0; i < attemptsCount; i += 1) {
     const [question, correctAnswer] = getGameData();
     console.log(`Question: ${question}`);
     const userAnswer = readlineSync.question('Your answer: ');
-    if (userAnswer === correctAnswer) {
-      console.log('Correct!');
-    } else {
-      return `Sorry, "${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".\nLet's try again, ${userName}!`;
+    if (userAnswer !== correctAnswer) {
+      console.log(`Sorry, "${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}".`);
+      console.log(`Let's try again, ${userName}!`);
+      return;
     }
+    console.log('Correct!');
   }
-  return `Congratulations, ${userName}!`;
+  console.log(`Congratulations, ${userName}!`);
 };
 
 export default runGame;
