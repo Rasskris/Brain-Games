5787a673a671be907574bce37aa3f384e2499fcd
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-05 19:40:14 +0300

    remove return, add console.log

--- src/games/greeting.js
+++ src/games/greeting.js
@@ -3,6 +3,6 @@ import readlineSync from 'readline-sync';
 const greeting = () => {
   console.log('Welcome to the Brain Games!\n');
   const userName = readlineSync.question('May I have you name? ');
-  return `Hello, ${userName}`;
+  console.log(`Hello, ${userName}`);
 };
 export default greeting;
