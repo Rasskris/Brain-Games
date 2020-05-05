9d52d1d30c97ad240d1fb08080c3a65db530d866
Rassoshenko Kristina <rassoshenko.kristina@yandex.ru>
2020-05-05 19:35:27 +0300

    fix code review remarks

--- src/games/calc.js
+++ src/games/calc.js
@@ -6,8 +6,8 @@ const description = 'What is the result of the expression?';
 const getGameData = () => {
   const operators = ['+', '-', '*'];
   const operator = operators[_.random(0, operators.length - 1)];
-  const firstNum = _.random(100);
-  const secondNum = _.random(100);
+  const firstNum = _.random(1, 100);
+  const secondNum = _.random(1, 100);
   const question = `${firstNum} ${operator} ${secondNum}`;
   let correctAnswer;
   switch (operator) {
